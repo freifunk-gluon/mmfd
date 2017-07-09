cd590263573b489430f999e65dcece4445851bae
Christof Schulze <christof.schulze@gmx.net>
2017-07-09 13:47:10 +0200

    rename neighbor to neighbour and fix many whitespaces (#2)
    * change neighbor into neighbour, fix whitespaces
    
    * fixup! change neighbor into neighbour, fix whitespaces

--- src/babeld.c
+++ src/babeld.c
@@ -1,9 +1,9 @@
 // connect to babeld
 // reconnect
-// notifiy about client changes
+// notify about client changes
 
 #include "babeld.h"
-#include "neighbor.h"
+#include "neighbour.h"
 #include "error.h"
 
 #include <unistd.h>
@@ -14,123 +14,119 @@
 #define CHUNKSIZE 1024
 
 void babeld_parse_line(struct context *ctx, char *line) {
-  char *action = NULL;
-  char *address_str = NULL;
-  char *ifname = NULL;
-  int reach, cost;
+	char *action = NULL;
+	char *address_str = NULL;
+	char *ifname = NULL;
+	int reach, cost;
 
-  print_neighbors(ctx);
+	print_neighbours(ctx);
 
-  int n = sscanf(line, "%ms neighbour %*x address %ms if %ms reach %x rxcost %*d txcost %*d cost %d",
-    &action, &address_str, &ifname, &reach, &cost);
+	int n = sscanf(line, "%ms neighbour %*x address %ms if %ms reach %x rxcost %*d txcost %*d cost %d", &action, &address_str, &ifname, &reach, &cost);
 
-  if (n != 5)
-  {
-	  free(action);
-	  free(address_str);
-	  free(ifname);
-    n = sscanf(line, "%ms neighbour %*x address %ms if %ms "
-	"reach %x rxcost %*d txcost %*d rtt %*f rttcost %*d cost %d",
-	&action, &address_str, &ifname, &reach, &cost);
-    if (n != 5)
-    {
-	  free(action);
-	  free(address_str);
-	  free(ifname);
-      n = sscanf(line, "%ms neighbour %*s", &action);
-      if (n == 2 )
-	printf("Received changes on a neighbour but could not match them on any of the neighbor-patterns. Exiting parser %d. This is a bug that should be reported.\n", n);
-      goto end;
-    }
-  }
-  struct in6_addr address;
+	if (n != 5) {
+		free(action);
+		free(address_str);
+		free(ifname);
+		n = sscanf(line, "%ms neighbour %*x address %ms if %ms reach %x rxcost %*d txcost %*d rtt %*f rttcost %*d cost %d", &action, &address_str, &ifname, &reach, &cost);
+		if (n != 5) {
+			free(action);
+			free(address_str);
+			free(ifname);
+			n = sscanf(line, "%ms neighbour %*s", &action);
+			if (n == 2 ) {
+				printf("Received changes on a neighbour but could not match them on any of the neighbour-patterns. Exiting parser %d. This is a bug that should be reported.\n", n);
+			}
 
-  if (inet_pton(AF_INET6, address_str, &address) != 1)
-  {
-    printf("received garbage instead of IPv6-address, not parsing line from babeld\n");
-    goto end;
-  }
+			goto end;
+		}
+	}
+	struct in6_addr address;
 
-  if (strcmp(action, "add") == 0)
-    neighbor_add(ctx, &address, ifname, reach, cost);
+	if (inet_pton(AF_INET6, address_str, &address) != 1) {
+		printf("received garbage instead of IPv6-address, not parsing line from babeld\n");
+		goto end;
+	}
 
-  if (strcmp(action, "change") == 0)
-    neighbor_change(ctx, &address, ifname, reach, cost);
+	if (strcmp(action, "add") == 0)
+		neighbour_add(ctx, &address, ifname, reach, cost);
 
-  if (strcmp(action, "flush") == 0)
-    neighbor_flush(ctx, &address, ifname);
+	if (strcmp(action, "change") == 0)
+		neighbour_change(ctx, &address, ifname, reach, cost);
+
+	if (strcmp(action, "flush") == 0)
+		neighbour_flush(ctx, &address, ifname);
 
 end:
-  free(action);
-  free(address_str);
-  free(ifname);
+	free(action);
+	free(address_str);
+	free(ifname);
 }
 
 bool babeld_handle_in(struct context *ctx, int fd) {
-  size_t old_len = ctx->babeld_buffer == NULL ? 0 : strlen(ctx->babeld_buffer);
-  size_t new_len = old_len + CHUNKSIZE + 1;
+	size_t old_len = ctx->babeld_buffer == NULL ? 0 : strlen(ctx->babeld_buffer);
+	size_t new_len = old_len + CHUNKSIZE + 1;
 
-  ctx->babeld_buffer = realloc(ctx->babeld_buffer, new_len);
+	ctx->babeld_buffer = realloc(ctx->babeld_buffer, new_len);
 
-  if (ctx->babeld_buffer == NULL)
-    exit_errno("Cannot allocate buffer");
+	if (ctx->babeld_buffer == NULL)
+		exit_errno("Cannot allocate buffer");
 
-  ssize_t len = read(fd, ctx->babeld_buffer + old_len, CHUNKSIZE);
+	ssize_t len = read(fd, ctx->babeld_buffer + old_len, CHUNKSIZE);
 
-  if (len == 0)
-    return false;
+	if (len == 0)
+		return false;
 
-  if (len == -1 && errno == EAGAIN)
-    return true;
+	if (len == -1 && errno == EAGAIN)
+		return true;
 
-  ctx->babeld_buffer[old_len + len] = 0;
+	ctx->babeld_buffer[old_len + len] = 0;
 
-  char *stringp, *line;
+	char *stringp, *line;
 
-  while (1) {
-    stringp = ctx->babeld_buffer;
-    line = strsep(&stringp, "\n");
+	while (1) {
+		stringp = ctx->babeld_buffer;
+		line = strsep(&stringp, "\n");
 
-    if ((ctx->verbose) && (strlen(line) > 1))
-      printf("about to parse line: %s\n", line);
+		if ((ctx->verbose) && (strlen(line) > 1))
+			printf("about to parse line: %s\n", line);
 
-    if (stringp == NULL)
-      break; // no line found
+		if (stringp == NULL)
+			break; // no line found
 
-    babeld_parse_line(ctx, line);
-    memmove(ctx->babeld_buffer, stringp, strlen(stringp) + 1);
-    ctx->babeld_buffer = realloc(ctx->babeld_buffer, strlen(ctx->babeld_buffer) + 1);
+		babeld_parse_line(ctx, line);
+		memmove(ctx->babeld_buffer, stringp, strlen(stringp) + 1);
+		ctx->babeld_buffer = realloc(ctx->babeld_buffer, strlen(ctx->babeld_buffer) + 1);
 
-    if (ctx->babeld_buffer == NULL)
-      exit_errno("Cannot allocate buffer");
-  }
+		if (ctx->babeld_buffer == NULL)
+			exit_errno("Cannot allocate buffer");
+	}
 
-  return true;
+	return true;
 }
 
 int babeld_connect(int port) {
-  int fd = socket(AF_INET6, SOCK_STREAM | SOCK_NONBLOCK, 0);
+	int fd = socket(AF_INET6, SOCK_STREAM | SOCK_NONBLOCK, 0);
 
-  if (fd < 0)
-    exit_errno("Unable to create TCP socket");
+	if (fd < 0)
+		exit_errno("Unable to create TCP socket");
 
-  struct sockaddr_in6 serveraddr = {
-    .sin6_family = AF_INET6,
-    .sin6_port = htons(port)
-  };
+	struct sockaddr_in6 serveraddr = {
+		.sin6_family = AF_INET6,
+		.sin6_port = htons(port)
+	};
 
-  if (inet_pton(AF_INET6, "::1", &serveraddr.sin6_addr.s6_addr) != 1)
-    exit_errno("Cannot parse hostname");
+	if (inet_pton(AF_INET6, "::1", &serveraddr.sin6_addr.s6_addr) != 1)
+		exit_errno("Cannot parse hostname");
 
-  if (connect(fd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) != 0) {
-    if (errno != EINPROGRESS)
-      exit_errno("Can not connect to babeld");
-  }
+	if (connect(fd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) != 0) {
+		if (errno != EINPROGRESS)
+			exit_errno("Can not connect to babeld");
+	}
 
-  // TODO: this is not a good idea? maybe we could receive EAGAIN!
-  if (send(fd, "monitor\n", 8, 0) != 8) {
-    exit_errno("Error while subscribing to babels events");
-  }
+	// TODO: this is not a good idea? maybe we could receive EAGAIN!
+	if (send(fd, "monitor\n", 8, 0) != 8) {
+		exit_errno("Error while subscribing to babels events");
+	}
 
-  return fd;
+	return fd;
 }
---
+++ src/neighbour.c
@@ -0,0 +1,92 @@
+#include "neighbour.h"
+#include "mmfd.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+
+void print_neighbours(struct context *ctx) {
+	puts("Neighbors:");
+
+	for (int i = 0; i < VECTOR_LEN(ctx->neighbours); i++) {
+		struct neighbour *neighbour = &VECTOR_INDEX(ctx->neighbours, i);
+
+		char ip_str[INET6_ADDRSTRLEN];
+		inet_ntop(AF_INET6, &neighbour->address.sin6_addr, ip_str, INET6_ADDRSTRLEN);
+
+		printf(" - %s on %s (%d), reach %d, cost %d\n", ip_str, neighbour->ifname, neighbour->address.sin6_scope_id, neighbour->reach, neighbour->cost);
+	}
+}
+
+bool cmp_neighbour(struct neighbour *neighbour, struct in6_addr *address, char *ifname) {
+	return strcmp(ifname, neighbour->ifname) == 0 && memcmp(address, &neighbour->address.sin6_addr, sizeof(*address)) == 0;
+}
+
+struct neighbour *find_neighbour(struct context *ctx, struct in6_addr *address, char *ifname) {
+	for (int i = 0; i < VECTOR_LEN(ctx->neighbours); i++) {
+		struct neighbour *neighbour = &VECTOR_INDEX(ctx->neighbours, i);
+
+		if (cmp_neighbour(neighbour, address, ifname))
+			return neighbour;
+	}
+
+	return NULL;
+}
+
+struct neighbour *add_neighbour(struct context *ctx, struct in6_addr *address, char *ifname) {
+	struct neighbour neighbour = {
+		.ifname = strdup(ifname),
+		.address = {
+			.sin6_family = AF_INET6,
+			.sin6_addr = *address,
+		}
+	};
+
+	VECTOR_ADD(ctx->neighbours, neighbour);
+	return &VECTOR_INDEX(ctx->neighbours, VECTOR_LEN(ctx->neighbours) - 1);
+}
+
+void remove_neighbour(struct context *ctx, struct in6_addr *address, char *ifname) {
+	for (int i = 0; i < VECTOR_LEN(ctx->neighbours); i++) {
+		struct neighbour *neighbour = &VECTOR_INDEX(ctx->neighbours, i);
+
+		if (cmp_neighbour(neighbour, address, ifname)) {
+			free(neighbour->ifname);
+			VECTOR_DELETE(ctx->neighbours, i);
+			break;
+		}
+	}
+}
+
+void neighbour_add(struct context *ctx, struct in6_addr *address, char *ifname, int reach, int cost) {
+	unsigned int ifindex = if_nametoindex(ifname);
+
+	if (ifindex == 0) {
+		perror("Cannot find interface for neighbour");
+		return;
+	}
+
+	struct neighbour *neighbour = add_neighbour(ctx, address, ifname);
+	neighbour->address.sin6_scope_id = ifindex;
+	neighbour->address.sin6_port = htons(PORT);
+}
+
+void neighbour_change(struct context *ctx, struct in6_addr *address, char *ifname, int reach, int cost) {
+	struct neighbour *neighbour = find_neighbour(ctx, address, ifname);
+
+	if (neighbour == NULL)
+	{
+		if ( ctx->verbose )
+			printf("did not find changed neighbour, adding\n");
+		neighbour_add(ctx,address, ifname, reach, cost);
+		return;
+	}
+	neighbour->reach = reach;
+	neighbour->cost = cost;
+}
+
+void neighbour_flush(struct context *ctx, struct in6_addr *address, char *ifname) {
+	remove_neighbour(ctx, address, ifname);
+}

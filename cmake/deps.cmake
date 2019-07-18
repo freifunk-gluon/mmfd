80c582869a2e4687c2a5dddce73708d3f8888bdd
Christof Schulze <christof.schulze@gmx.net>
2019-07-18 09:49:34 +0200

    Dockerfile: include libjson-c, remove old unused dockerfile from root of repository

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -2,5 +2,3 @@ INCLUDE(FindPkgConfig)
 IF(NOT PKG_CONFIG_FOUND)
 	MESSAGE(FATAL_ERROR "Error: pkg-config not found on this system")
 ENDIF(NOT PKG_CONFIG_FOUND)
-
-# find_package(libbabelhelper REQUIRED)
